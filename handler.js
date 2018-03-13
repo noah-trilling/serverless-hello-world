50425a59bb2676a789997f22e156cc2b95c9beba
Noah Trilling <noah.trilling@gmail.com>
2018-03-12 22:11:01 -0500

    first commit. serverless hello world application

---
+++ handler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.helloName = (event, context, callback) => {
+  let name = 'stranger';
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name;
+  }
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: `Hello, ${name}!`
+    })
+  };
+
+  callback(null, response);
+};
